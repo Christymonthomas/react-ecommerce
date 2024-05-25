1acab24636437580ffd537a296f8d81a24438700
Christy <christymonthomas1999@gmail.com>
2024-05-25 13:24:51 +0530

    Added simple routes/controlller/middleware

---
+++ server/controlllers/auth.js
@@ -0,0 +1,5 @@
+export const users = async (req, res) => {
+  res.json({
+    data: "Christy Thomas says hi from controller",
+  });
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 // Database connection
 
@@ -16,11 +17,9 @@ mongoose
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  res.json({
-    data: "Christy Thomas Hiii",
-  });
-});
+//router middleware
+
+app.use("/api", authRoutes);
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
