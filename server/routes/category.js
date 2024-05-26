7a2665eeccf7ad82d91e2b404e6d2c43c9ff5859
Christy <christymonthomas1999@gmail.com>
2024-05-26 17:41:59 +0530

    Added CURD for categories

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,17 @@
 import express from "express";
 import { requireSignin, isAdmin } from "../middlewars/auth.js";
-import { create } from "../controlllers/category.js";
+import {
+  create,
+  update,
+  remove,
+  list,
+  read,
+} from "../controlllers/category.js";
 const router = express.Router();
 router.post("/category", requireSignin, isAdmin, create);
+router.put("/category/:categoryId", requireSignin, isAdmin, update);
+router.delete("/category/:categoryId", requireSignin, isAdmin, remove);
+router.get("/category", list);
+router.get("/category/:slug", read);
 
 export default router;
