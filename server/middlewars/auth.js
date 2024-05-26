b41709dd00e065f5cb6a03273cf49cfe70127e82
Christy <christymonthomas1999@gmail.com>
2024-05-26 10:17:25 +0530

    Added middleware to check the logged user is admin or not

--- server/middlewars/auth.js
+++ server/middlewars/auth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 export const requireSignin = (req, res, next) => {
   //   console.log("REQUEST HEADES=>", req.headers);
@@ -13,5 +14,18 @@ export const requireSignin = (req, res, next) => {
     next();
   } catch (error) {
     return res.status(401).json(error);
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user.role !== 1) {
+      res.status(401).send("Unauthorized");
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { register, login, secret } from "../controlllers/auth.js";
-import { requireSignin } from "../middlewars/auth.js";
+import { requireSignin, isAdmin } from "../middlewars/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 //testing
-router.get("/secret", requireSignin, secret);
+router.get("/secret", requireSignin, isAdmin, secret);
 
 export default router;
