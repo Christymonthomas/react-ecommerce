fb3997e85b1b7a736a7b9c4db0cd1a3b1c326ae8
Christy <christymonthomas1999@gmail.com>
2024-05-28 22:06:43 +0530

    Added various product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,8 +1,21 @@
 import express from "express";
 import formidable from "express-formidable";
 import { requireSignin, isAdmin } from "../middlewars/auth.js";
-import { create } from "../controlllers/product.js";
+import {
+  create,
+  list,
+  read,
+  photo,
+  remove,
+  update,
+} from "../controlllers/product.js";
 const router = express.Router();
 router.post("/product", requireSignin, isAdmin, formidable(), create);
+router.get("/products", list);
+
+router.get("/product/:slug", read);
+router.get("/product/photo/:productId", photo);
+router.delete("/product/:productId", requireSignin, isAdmin, remove);
+router.put("/product/:productId", requireSignin, isAdmin, formidable(), update);
 
 export default router;
